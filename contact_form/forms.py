26556026d36d5aabb9508bcd2eabe5567c5ad331
polinamalyhina <polja.malygina@gmail.com>
2023-04-01 20:20:58 +0300

    upgrade

--- contact_form/forms.py
+++ contact_form/forms.py
@@ -1,18 +1,16 @@
 from django.forms import ModelForm
-from django.forms import Textarea
+from django.forms import Textarea, TextInput, EmailInput
 from .models import Contacts
 
 
 class ContactForm(ModelForm):
 
     class Meta:
-        # Определяем модель, на основе которой создаем форму
         model = Contacts
-        # Поля, которые будем использовать для заполнения
-        fields = ['name', 'email', 'message']
+        fields = '__all__'
         widgets = {
+            'name': TextInput(attrs={'placeholder': 'Name'}),
+            'email': EmailInput(attrs={'placeholder': 'Email'}),
             'message': Textarea(
-                attrs={
-                    'placeholder': 'Please, give a feedback'
-                })
+                attrs={'placeholder': 'Please, give a feedback'})
         }
